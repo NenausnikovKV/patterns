cd5ee85b05f7fd7b3ef79beb8df0f4a1250cdc14
Konstantin Nenausnikov <konstantin2113@mail.ru>
2024-08-09 16:23:20 +0300

    remove simple factory

--- general_patterns/test/test_pizza.py
+++ general_patterns/test/test_pizza.py
@@ -6,20 +6,7 @@
 import pytest
 
 from general_patterns.pizza_fabric.pizza_exception import WrongPizzaType
-from general_patterns.pizza_fabric.pizza_fabric import SimplePizzaFactory
-from general_patterns.pizza_fabric.pizza_store import OldPizzaStore, NYStylePizzaStore, \
-    ChicagoStylePizzaStore
-
-
-def test_pizza():
-    """Тест заказа пиццы"""
-    pizza_store = OldPizzaStore(pizza_factory=SimplePizzaFactory())
-    pizza_store.order_pizza("cheese")
-    pizza_store.order_pizza("pepperoni")
-    pizza_store.order_pizza("clam")
-    pizza_store.order_pizza("veggie")
-    with pytest.raises(WrongPizzaType):
-        pizza_store.order_pizza("wrong_type_of_pizza")
+from general_patterns.pizza_fabric.pizza_store import NYStylePizzaStore, ChicagoStylePizzaStore
 
 
 def test_ny_pizza():
